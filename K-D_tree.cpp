e1221406b1cbebbee37e98ca8a60aed8cd5595df
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2022-12-10 14:40:53 +0600

    min value search done

--- K-D_tree.cpp
+++ K-D_tree.cpp
@@ -77,12 +77,46 @@ bool searchNode(node *root, vector<double> p, int depth)
     }
 }
 
+double minimum(double a, double b, double c)
+{
+    return min(a, min(b, c));
+}
+
+double findMin(node *root, int d, int depth)
+{
+    if (root == NULL)
+    {
+        return 100000000.00;
+    }
+
+    int cut_dimension = depth % k;
+
+    if (cut_dimension == d)
+    {
+        if (root->left == NULL)
+        {
+            return root->point[d];
+        }
+        else
+        {
+            return min(root->point[d], findMin(root->left, d, depth + 1));
+        }
+    }
+
+    return minimum(root->point[d], findMin(root->left, d, depth + 1), findMin(root->right, d, depth + 1));
+}
+
 int main(void)
 {
+    // cout << "Enter the tree dimension: ";
+    // cin >> k;
+
+    cout << "\nEnter total number of nodes and edges: " << endl;
     freopen("input.txt", "r", stdin);
     int n;
     cin >> n;
 
+    // tree creation
     struct node *root = NULL;
     vector<double> p;
     for (int i = 0; i < n; i++)
@@ -95,6 +129,7 @@ int main(void)
         root = insertNode(root, p, 0);
     }
 
+    // searching a node
     vector<double> p1;
     p1.push_back(7);
     p1.push_back(8.1);
@@ -107,6 +142,10 @@ int main(void)
     {
         cout << "Not Found" << endl;
     }
+
+    // finding minimum value in a dimension
+    int d = 1; // input dimension
+    cout << "Min value in dimension " << d << " is: " << findMin(root, d, 0);
 
     return 0;
 }
